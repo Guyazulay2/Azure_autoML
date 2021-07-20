4d7b70a3c21371521fd604c2a72d7fee79271c50
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-07-20 14:32:10 +0300

    upload new files

---
+++ Azure_Automationn.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from azureml.core import Workspace, Dataset
+
+
+subscription_id = '05ed07e3-df05-46ff-8687-8555449022bb'
+resource_group = 'ML-resources'
+workspace_name = 'ML-resources'
+
+workspace = Workspace(subscription_id, resource_group, workspace_name)
+
+dataset = Dataset.get_by_name(workspace, name='CZ-FPZ_train_data')
+dataset.to_pandas_dataframe()
+
+
+# In[2]:
+
+
+from azureml.core.experiment import Experiment
+from azureml.core.workspace import Workspace
+import pandas as pd
+
+ws = Workspace.from_config()
+experiment_name = 'test'
+
+experiment = Experiment(ws, experiment_name)
+
+output = {}
+output['Subscription ID'] = ws.subscription_id
+output['Workspace'] = ws.name
+output['Resource Group'] = ws.resource_group
+output['Location'] = ws.location
+output['Experiment Name'] = experiment.name
+pd.set_option('display.max_colwidth', -1)
+outputDf = pd.DataFrame(data = output, index= [''])
+outputDf.T
+
+
+# In[3]:
+
+
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
+
+cpu_cluster_name = "GPU-compute"
+
+try:
+    compute_target = ComputeTarget(workspace=ws, name=cpu_cluster_name)
+    print("Found exsist cluster, Use it ! ")
+except ComputeTargetException:
+    compute_config = AmlCompute.provisioning_configuration(vm_size='STANDART_D2_V2', max_nodes=6)
+
+    compute_target = ComputeTarget.create(ws, cpu_cluster_name, compute_config)
+
+compute_target.wait_for_completion(show_output=True)
+
+
+# In[4]:
+
+
+from azureml.train.automl import AutoMLConfig
+import logging
+
+
+automl_settings = {
+    "experiment_timeout_hours": 0.2,
+    "enable_early_stopping": True,
+    "iteration_timeout_minutes": 5,
+    "max_concurrent_iterations": 1,
+    "max_cores_per_iteration": 1,
+    "featurization": 'auto',
+    "verbosity": logging.INFO,
+}
+
+automl_config = AutoMLConfig(task = 'classification',
+                             primary_metric= 'AUC_weighted',
+                             compute_target=compute_target,
+                             blocked_models=['TensorFlowLinearClassifier'],
+                             label_column_name = '0',
+                             training_data = dataset,
+                             n_cross_validations=2,
+                             **automl_settings
+                            )
+
+
+# In[1]:
+
+
+remote_run = experiment.submit(automl_config, show_output = False)
+
+
+# In[5]:
+
+
+from azureml.widgets import RunDetails
+remote_run.wait_for_completion(show_output=True)
+
+
+# In[4]:
+
+
+best_run, fitted_model = remote_run.get_output()
+print(best_run)
+print(fitted_model)
+
+
+# In[3]:
+
+
+from azureml.widgets import RunDetails
+RunDetails(remote_run).show() 
+
+
+# In[2]:
+
+
+# Wait for the best model explanation run to complete
+from azureml.interpret import ExplanationClient
+from azureml.core.run import Run
+
+model_explainability_run_id = remote_run.id + "_" + "ModelExplain"
+print(model_explainability_run_id)
+model_explainability_run = Run(experiment=experiment, run_id=model_explainability_run_id)
+model_explainability_run.wait_for_completion()
+
+# Get the best run object
+best_run, fitted_model = remote_run.get_output()
+
+
+# In[12]:
+
+
+from azureml.interpret import ExplanationClient
+
+client = ExplanationClient.from_run(best_run)
+engineered_explanations = client.download_model_explanation(raw=True)
+exp_data = engineered_explanations.get_feature_importance_dict()
+exp_data
+
+
+# In[13]:
+
+
+best_run, fitted_model = remote_run.get_output()
+
+
+# In[15]:
+
+
+model_name = best_run.properties['model_name']
+
+script_file_name = 'inference/score.py'
+
+best_run.download_file('outputs/scoring_file_v_1_0_0.py', 'inference/score.py')
+
